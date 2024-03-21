b38b081eb73a3cd459789eef803226fbbc5fc2c5
LazarosZiskos <ziskoslaz@gmail.com>
2024-03-21 20:45:18 +0200

    finished newest products section

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -3,6 +3,7 @@ import { client } from "../lib/sanity";
 import { simplifiedProduct } from "../interface";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 
 async function getData() {
   const query = `*[_type == 'product'][0...4] | order(_createdAt desc) {
@@ -29,11 +30,42 @@ const Newest = async () => {
             Our Newest Products
           </h2>
           <Link href="/all" className="text-primary flex items-center gap-x-1">
-            See all
+            See all{" "}
             <span>
               <ArrowRight />
             </span>
           </Link>
+        </div>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {data.map((product) => (
+            <div key={product._id} className="group relative">
+              <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
+                <Image
+                  src={product.imageUrl}
+                  alt="product image"
+                  width={500}
+                  height={500}
+                  className="w-full h-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700 font-semibold">
+                    <Link href={`/products/${product.slug}`}>
+                      {product.name}
+                    </Link>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">
+                    {product.categoryName}
+                  </p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">
+                  ${product.price}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
