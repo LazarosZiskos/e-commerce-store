5d1ccec73c4017dcffddd7692d276de84c0a2d68
LazarosZiskos <ziskoslaz@gmail.com>
2024-03-22 17:26:21 +0200

    finished category and product routes/pages

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -1,9 +1,51 @@
+"use client";
+import Image from "next/image";
+import { urlFor } from "../lib/sanity";
+import { useState } from "react";
+
 interface iAppProps {
   images: any;
 }
 
 const ImageGallery = ({ images }: iAppProps) => {
-  return <div>ImageGallery</div>;
+  const [bigImage, setBigImage] = useState(images[0]);
+
+  const handleImages = (image: any) => {
+    setBigImage(image);
+  };
+
+  return (
+    <div className="grid gap-4 lg:grid-cols-5">
+      <div className="order-last flex gap-4 lg:order-none lg:flex-col">
+        {images.map((image: any, idx: any) => (
+          <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
+            <Image
+              src={urlFor(image).url()}
+              width={200}
+              height={200}
+              alt="photo"
+              className="h-full w-full object-cover object-center cursor-pointer"
+              priority
+              onClick={() => handleImages(image)}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4">
+        <Image
+          src={urlFor(bigImage).url()}
+          alt="big-product-image"
+          width={500}
+          height={500}
+          className="h-full w-full object-cover object-center"
+          priority
+        />
+        <span className="absolute left-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-bold uppercase tracking-wider text-white">
+          sale
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default ImageGallery;
--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import ImageGallery from "@/app/components/ImageGallery";
 import { fullProduct } from "@/app/interface";
 import { client } from "@/app/lib/sanity";
-import { Images } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Star, Truck } from "lucide-react";
 
 async function getData(slug: string) {
   const query = `*[_type == 'product' && slug.current == '${slug}'][0] {
@@ -26,7 +27,55 @@ const ProductPage = async ({ params }: { params: { slug: string } }) => {
     <div className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 md:px-8">
         <div className="grid gap-8 md:grid-cols-2">
-          <ImageGallery images={Images} />
+          <ImageGallery images={data.images} />
+
+          <div className="md:py-8">
+            <div className="mb-2 md:mb-3">
+              <span className="mb-0.5 inline-block text-gray-500">
+                {data.categoryName}
+              </span>
+              <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
+                {data.name}
+              </h2>
+            </div>
+
+            <div className="mb-6 flex items-center gap-3 md:mb-10">
+              <Button className="space-x-2 rounded-full">
+                <span className="text-sm">4.2</span>
+                <Star className="h-5 w-5" />
+              </Button>
+              <span className="text-sm text-gray-500 ">56 Ratings</span>
+            </div>
+
+            <div className="mb-4">
+              <div className="flex items-end gap-2">
+                <span className="text-xl font-bold text-gray-800 md:text-2xl">
+                  ${data.price}
+                </span>
+                <span className="line-through mb-0.5 text-red-500">
+                  ${data.price + 50}
+                </span>
+              </div>
+
+              <span className="text-sm text-gray-500">
+                Incl. Vat plus shipping
+              </span>
+            </div>
+
+            <div className="mb-6 flex items-center gap-2 text-gray-500">
+              <Truck />
+              <span className="text-sm">2-4 Day Shipping</span>
+            </div>
+
+            <div className="flex gap-2.5">
+              <Button>Add To Cart</Button>
+              <Button variant="secondary">Checkout now</Button>
+            </div>
+
+            <p className="mt-12 text-base text-gray-500 tracking-wide">
+              {data.description}
+            </p>
+          </div>
         </div>
       </div>
     </div>
