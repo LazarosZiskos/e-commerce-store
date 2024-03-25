d5b8aaabffc464fc8772c0704f082c28e0613fad
LazarosZiskos <ziskoslaz@gmail.com>
2024-03-25 19:20:15 +0200

    project done.

--- app/stripe/error/page.tsx
+++ app/stripe/error/page.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
-const page = () => {
-  return <div>page</div>;
+const ErrorStripe = () => {
+  return (
+    <div className="py-10">
+      <h2>Something went wrong...</h2>
+    </div>
+  );
 };
 
-export default page;
+export default ErrorStripe;
